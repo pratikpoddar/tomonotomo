fd7b156856cb67ca8726313218d649a35a2b4d3f
Pratik Poddar <pratik.phodu@gmail.com>
2013-08-15 08:14:02 +0000

    Email settings

--- tomonotomo_project/settings.py
+++ tomonotomo_project/settings.py
@@ -4,15 +4,16 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Pratik Poddar', 'pratik.phodu@gmail.com'),
 )
 
+EMAIL_SUBJECT_PREFIX = "[Django - Tomonotomo] "
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/himangshu/pratik/databasefile',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/ubuntu/databasefile',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
