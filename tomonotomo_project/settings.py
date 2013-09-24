33aa116977b7f6c7b739ab3d377b4e6e710a7390
Pratik Poddar <pratik.phodu@gmail.com>
2013-09-24 17:26:08 +0000

    Cookies Management

--- tomonotomo_project/settings.py
+++ tomonotomo_project/settings.py
@@ -38,6 +38,7 @@ DATABASES = {
     }
 }
 
+SESSION_COOKIE_DOMAIN = '.tomonotomo.com'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
