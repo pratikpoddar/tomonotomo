351723dbb6bb62f089fb541344e566ff64559a9d
Pratik Poddar <pratik.phodu@gmail.com>
2013-09-28 22:14:02 +0000

    Minor

--- tomonotomo/views.py
+++ tomonotomo/views.py
@@ -524,7 +524,7 @@ def dbsummary(request):
 	'dbsummary_userrelstatus': UserTomonotomo.objects.values('relstatus').annotate(Count('relstatus')),
 	'dbsummary_users': UserTomonotomo.objects.exclude(email=None).values('userid','first_name','last_name','email'),
 	'dbsummary_quota': UserQuota.objects.exclude(quota=30).values('userid','quota'),
-	'dbsummary_quota_verification': UserFeedback.objects.filter(timestamp__gte=date.today()).values('userid').annotate(Count('fbid')),
+	'dbsummary_quota_verification': UserFeedback.objects.filter(timestamp__gte=date.today()).values('userid').annotate(Count('fbid', distinct=True)),
 	'dbsummary_dbchecksstring': dbchecks.dbchecks2(),
 	'dbsummary_feedback': UserFeedback.objects.values('action').annotate(Count('action')).order_by(),
 	'dbsummary_users_login_24': UserLogin.objects.filter(timestamp__gte=datetime.now()+timedelta(hours=-24)).order_by('timestamp').values('userlogin','timestamp'),
