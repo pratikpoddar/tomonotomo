bd46d4b40fbac397d4f3a841eaa0ca70f5e6bec1
Pratik Poddar <pratik.phodu@gmail.com>
2013-09-29 10:44:57 +0000

    Error Reporting

--- tomonotomo_project/settings.py
+++ tomonotomo_project/settings.py
@@ -12,6 +12,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pratik.phodu@gmail.com'
+EMAIL_HOST_PASSWORD = 'indiarocks'
+
 FAILED_RUNS_CRONJOB_EMAIL_PREFIX = "[Django - Tomonotomo - Cron Job] "
 EMAIL_SUBJECT_PREFIX = "[Django - Tomonotomo] "
 
