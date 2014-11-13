3c4cf9d34df8a10a1d1d3569c5d0a504833793ba
Pratik Poddar <pratik.phodu@gmail.com>
2014-11-13 13:29:26 +0000

    Tabs vs spaces problem

--- tomonotomo/social_auth_pipeline.py
+++ tomonotomo/social_auth_pipeline.py
@@ -33,11 +33,11 @@ def create_custom_user(backend, details, user=None, user_exists=UserSocialAuth.s
 
 	## TODO: Make not updating condition stricter. stop only if (not new) and (updated in last 10 days)
 	if kwargs['is_new'] == False:
-        	logger.debug("social_auth_pipeline.create_custom_user - Returning user " + str(user))
+		logger.debug("social_auth_pipeline.create_custom_user - Returning user " + str(user))
 	else:
 		logger.debug("social_auth_pipeline.create_custom_user - Getting data for first time user " + str(user))
 
-        if user is None:
+	if user is None:
 		logger.exception("social_auth_pipeline.create_custom_user - User came as None in the function create_custom_user")
 		return
 	if backend.__class__ != FacebookBackend:
@@ -50,7 +50,7 @@ def create_custom_user(backend, details, user=None, user_exists=UserSocialAuth.s
 
 	try:
 		profile = UserTomonotomo.objects.get(userid=res.get('id'))
-        except UserTomonotomo.DoesNotExist:
+	except UserTomonotomo.DoesNotExist:
 		profile = UserTomonotomo()
 
 	profile.accesstoken = res.get('access_token')
@@ -59,7 +59,7 @@ def create_custom_user(backend, details, user=None, user_exists=UserSocialAuth.s
 		profile.work= getSanitizedWork(res['work'])
 	if res.get('education'):
 		profile.education= getSanitizedEducation(res['education'])
-        profile.email = res.get('email')
+	profile.email = res.get('email')
 	if not res.get('email'):
 		profile.email = str(res.get('username'))+"@facebook.com"
 		logger.info("social_auth_pipeline.create_custom_user - Email not found for user " + str(res.get('id')) + ". Using " + profile.email)
@@ -163,20 +163,20 @@ def create_custom_user(backend, details, user=None, user_exists=UserSocialAuth.s
 	return
 
 def getSanitizedEducation (educationProfile):
-        """ Helper function to get education profile as a structured string """
-        education = ""
-        for value in educationProfile:
-                if value.get('school') and value['school'].get('name'):
-                        education = education + value['school']['name']+'---'
-        return education
+	""" Helper function to get education profile as a structured string """
+	education = ""
+	for value in educationProfile:
+		if value.get('school') and value['school'].get('name'):
+			education = education + value['school']['name']+'---'
+	return education
 
 def getSanitizedWork (workProfile):
-        """ Helper function to get work profile as a structured string """
-        work = ""
-        for value in workProfile:
-                if value.get('employer') and value['employer'].get('name'):
-                        work = work + value['employer']['name']+'---'
-        return work
+	""" Helper function to get work profile as a structured string """
+	work = ""
+	for value in workProfile:
+		if value.get('employer') and value['employer'].get('name'):
+			work = work + value['employer']['name']+'---'
+	return work
 
 def extractAllSanitizedLikes (likeResFBGraph):
 	
@@ -200,7 +200,7 @@ def getAllLikes (likelink):
 	return likesList
 
 def getSanitizedLikes (likesList):
-        """ Helper function to get likes as a structured string """
+	""" Helper function to get likes as a structured string """
 	likesResult = []
 	for likevalue in likesList:
 		try:
@@ -233,158 +233,158 @@ def saveLocation (locationid):
 def postProcessing(userid, accessToken):
 
 	userloggedin = UserTomonotomo.objects.get(userid=userid)
-        graph = GraphAPI(accessToken)
-        logger.debug("social_auth_pipeline.postProcessing - Processing " + accessToken + " - " + str(userid))
+	graph = GraphAPI(accessToken)
+	logger.debug("social_auth_pipeline.postProcessing - Processing " + accessToken + " - " + str(userid))
 	try:
 		## Checking if accesstoken is valid
-        	friendnumber = graph.fql('SELECT friend_count FROM user where uid=me()')
-        	numberfriends = friendnumber.get('data')[0].get('friend_count')
-        	logger.debug("social_auth_pipeline.postProcessing - number of friends " + str(numberfriends))
+		friendnumber = graph.fql('SELECT friend_count FROM user where uid=me()')
+		numberfriends = friendnumber.get('data')[0].get('friend_count')
+		logger.debug("social_auth_pipeline.postProcessing - number of friends " + str(numberfriends))
 	except Exception as e:
 		logger.exception("social_auth_pipeline.postProcessing - Accesstoken is invalid - " + str(e) + " - " + str(e.args))
 		raise
 
-        friendgraphdata= []
-        friendgraph= []
-        for i in range(0,10):
-            query = 'SELECT uid,first_name,last_name,username,name,birthday,education,work,sex,hometown_location,current_location,relationship_status FROM user WHERE uid in (SELECT uid2 FROM friend where uid1=me() limit '+str(max(0,(i*500)-100))+',500)'
-	    friendgraphoutput = graph.fql(query)
-	    if friendgraphoutput:
-	            friendgraphdata.append(friendgraphoutput)
-		    try:
-		            friendgraph.extend(friendgraphoutput.get('data'))
-			    logger.debug("social_auth_pipeline.postProcessing - received data for " + str(len(friendgraph)) + " friends - some are duplicate")
-		    except:
-			    pass
+	friendgraphdata= []
+	friendgraph= []
+	for i in range(0,10):
+		query = 'SELECT uid,first_name,last_name,username,name,birthday,education,work,sex,hometown_location,current_location,relationship_status FROM user WHERE uid in (SELECT uid2 FROM friend where uid1=me() limit '+str(max(0,(i*500)-100))+',500)'
+		friendgraphoutput = graph.fql(query)
+		if friendgraphoutput:
+			friendgraphdata.append(friendgraphoutput)
+			try:
+				friendgraph.extend(friendgraphoutput.get('data'))
+				logger.debug("social_auth_pipeline.postProcessing - received data for " + str(len(friendgraph)) + " friends - some are duplicate")
+			except:
+				pass
 
-        logger.debug("social_auth_pipeline.postProcessing - list of friends data "+str(len(friendgraph))+" - some are duplicate")
+	logger.debug("social_auth_pipeline.postProcessing - list of friends data "+str(len(friendgraph))+" - some are duplicate")
 
-        count = 0
+	count = 0
 	fqlerror = 0
-        for frienddata in friendgraph:
+	for frienddata in friendgraph:
 
-            count = count + 1
-	    try:
-	            logger.debug("social_auth_pipeline.postProcessing - Saving detailed information for friendid - " + str(frienddata.get('uid')) + " - count " + str(count))
-	    except:
-		    logger.exception("social_auth_pipeline.postProcessing - Saving detailed information for friendid without uid - " + str(frienddata) + " - count " + str(count))
-
-            try:
-		profilefriends = UserFriends.objects.get(userid=userloggedin, friendid=frienddata.get('uid'))
-            except UserFriends.DoesNotExist:
-                profilefriends = UserFriends()
-                profilefriends.userid = userloggedin
-                profilefriends.friendid = frienddata.get('uid')
+		count = count + 1
 		try:
-			if UserFriends.objects.filter(userid=userloggedin, friendid=frienddata.get('uid')).count()==0:
-				profilefriends.save()
+			logger.debug("social_auth_pipeline.postProcessing - Saving detailed information for friendid - " + str(frienddata.get('uid')) + " - count " + str(count))
+		except:
+			logger.exception("social_auth_pipeline.postProcessing - Saving detailed information for friendid without uid - " + str(frienddata) + " - count " + str(count))
+
+		try:
+			profilefriends = UserFriends.objects.get(userid=userloggedin, friendid=frienddata.get('uid'))
+		except UserFriends.DoesNotExist:
+			profilefriends = UserFriends()
+			profilefriends.userid = userloggedin
+			profilefriends.friendid = frienddata.get('uid')
+			try:
+				if UserFriends.objects.filter(userid=userloggedin, friendid=frienddata.get('uid')).count()==0:
+					profilefriends.save()
+			except Exception as e:
+				logger.exception("social_auth_pipeline.postProcessing - Exception saving userfriends data - error - " + str(frienddata.get('uid')) + " - " + str(e.args))
+				pass
+		except UserFriends.MultipleObjectsReturned:
+			logger.exception("social_auth_pipeline.postProcessing - Exception saving userfriends data - multiple entries in userfriends race error " + str(frienddata.get('uid')))
+			pass
 		except Exception as e:
-			logger.exception("social_auth_pipeline.postProcessing - Exception saving userfriends data - error - " + str(frienddata.get('uid')) + " - " + str(e.args))
-			pass
-	    except UserFriends.MultipleObjectsReturned:
-		logger.exception("social_auth_pipeline.postProcessing - Exception saving userfriends data - multiple entries in userfriends race error " + str(frienddata.get('uid')))
-            	pass
-	    except Exception as e:
-		try:
-			logger.exception("social_auth_pipeline.postProcessing - Exception saving UserFriends - " + str(frienddata) + " " + str(e.args))
-		except:
-			pass
+			try:
+				logger.exception("social_auth_pipeline.postProcessing - Exception saving UserFriends - " + str(frienddata) + " " + str(e.args))
+			except:
+				pass
 
-		logger.exception("social_auth_pipeline.postProcessing - Exception saving UserFriends - " + str(e) + " - " + str(e.args))
-		raise
-
-	    try:
-                userfriend = UserTomonotomo.objects.get(userid=frienddata.get('uid'))
-            except UserTomonotomo.DoesNotExist:
-                userfriend = UserTomonotomo()
-                userfriend.userid = frienddata.get('uid')
-
-	    try:	     
-            	if frienddata.get('work'):
-               		userfriend.work= getSanitizedWork(frienddata['work'])
-            	if frienddata.get('education'):
-                	userfriend.education= getSanitizedEducation(frienddata['education'])
-            	userfriend.first_name = frienddata.get('first_name')
-           	userfriend.last_name = frienddata.get('last_name')
-		try:
-            		userfriend.gender = genderdict[frienddata.get('sex') or "not specified"]
-		except:
-			userfriend.gender = genderdict["not specified"]
-           	if frienddata.get('hometown_location'):
-                	userfriend.hometown = frienddata.get('hometown_location').get('name')
-            	if frienddata.get('current_location'):
-                	userfriend.location = frienddata.get('current_location').get('name')
-			saveLocation(frienddata.get('current_location').get('id'))
+			logger.exception("social_auth_pipeline.postProcessing - Exception saving UserFriends - " + str(e) + " - " + str(e.args))
+			raise
 
 		try:
-			userfriend.relstatus = relstatusdict[frienddata.get('relationship_status') or "not specified"]
-		except:
-			userfriend.relstatus = relstatusdict["not specified"]
-
-            	userfriend.username = frienddata.get('username')
-            
-	    	if frienddata.get('birthday'):
-                	try:
-                    		userfriend.birthday = time.strftime("%m/%d/%Y", time.strptime(frienddata.get('birthday'), "%B %d, %Y"))
-                	except:
-                    		pass
+			userfriend = UserTomonotomo.objects.get(userid=frienddata.get('uid'))
+		except UserTomonotomo.DoesNotExist:
+			userfriend = UserTomonotomo()
+			userfriend.userid = frienddata.get('uid')
 
 		try:
-			# TODO: Gets interests only if interests are not there. Need to change this.
-			if (not userfriend.interests) and (not fqlerror):
-				query = 'SELECT page_id, name FROM page WHERE page_id IN (SELECT page_id FROM page_fan WHERE uid=' + str(frienddata.get('uid')) + ')'
-				userfriend.interests = pickle.dumps(graph.fql(query).get('data')).decode('latin1')
+			if frienddata.get('work'):
+				userfriend.work= getSanitizedWork(frienddata['work'])
+			if frienddata.get('education'):
+				userfriend.education= getSanitizedEducation(frienddata['education'])
+			userfriend.first_name = frienddata.get('first_name')
+			userfriend.last_name = frienddata.get('last_name')
+			try:
+				userfriend.gender = genderdict[frienddata.get('sex') or "not specified"]
+			except:
+				userfriend.gender = genderdict["not specified"]
+			if frienddata.get('hometown_location'):
+				userfriend.hometown = frienddata.get('hometown_location').get('name')
+			if frienddata.get('current_location'):
+				userfriend.location = frienddata.get('current_location').get('name')
+				saveLocation(frienddata.get('current_location').get('id'))
+
+			try:
+				userfriend.relstatus = relstatusdict[frienddata.get('relationship_status') or "not specified"]
+			except:
+				userfriend.relstatus = relstatusdict["not specified"]
+
+			userfriend.username = frienddata.get('username')
+		
+			if frienddata.get('birthday'):
+				try:
+					userfriend.birthday = time.strftime("%m/%d/%Y", time.strptime(frienddata.get('birthday'), "%B %d, %Y"))
+				except:
+					pass
+
+			try:
+				# TODO: Gets interests only if interests are not there. Need to change this.
+				if (not userfriend.interests) and (not fqlerror):
+					query = 'SELECT page_id, name FROM page WHERE page_id IN (SELECT page_id FROM page_fan WHERE uid=' + str(frienddata.get('uid')) + ')'
+					userfriend.interests = pickle.dumps(graph.fql(query).get('data')).decode('latin1')
+			except Exception as e:
+				fqlerror = 1
+				logger.exception("social_auth_pipeline.postProcessing - Error getting friends' likes - " + str(frienddata.get('uid')) + " - " + str(e.args))
+				pass
+
 		except Exception as e:
-			fqlerror = 1
-			logger.exception("social_auth_pipeline.postProcessing - Error getting friends' likes - " + str(frienddata.get('uid')) + " - " + str(e.args))
-			pass
+			logger.exception("social_auth_pipeline.postProcessing - Error collecting information - " + str(e) + " - " + str(e.args))
+			raise
 
-	    except Exception as e:
-		logger.exception("social_auth_pipeline.postProcessing - Error collecting information - " + str(e) + " - " + str(e.args))
-		raise
+		try:
+			if UserTomonotomo.objects.filter(userid=frienddata.get('uid')).count() == 0:
+				userfriend.save()
+		except Exception as e:
+				logger.exception("social_auth_pipeline.postProcessing - Error saving userdata - " + str(e) + " - " + str(e.args))
+				pass
 
-	    try:
-		if UserTomonotomo.objects.filter(userid=frienddata.get('uid')).count() == 0:
-			userfriend.save()
-	    except Exception as e:
-		logger.exception("social_auth_pipeline.postProcessing - Error saving userdata - " + str(e) + " - " + str(e.args))
-		pass
-
-        return
+	return
 
 class startPostProcessing(CronJobBase):
-    RUN_EVERY_MINS = 1 # every 1 min
+	RUN_EVERY_MINS = 1 # every 1 min
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'tomonotomo.social_auth_pipeline.startPostProcessing'    # a unique code
+	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+	code = 'tomonotomo.social_auth_pipeline.startPostProcessing' # a unique code
 
-    def do(self):
-	logger.debug("social_auth_pipeline.startPostProcessing - StartPostProcessing starts")
-        pendingusers = UserProcessing.objects.filter(entryaddtime__lte=datetime.now(pytz.timezone('America/Chicago'))+timedelta(minutes=-5)).values('userloggedin','accesstoken')
-	logger.debug("social_auth_pipeline.startPostProcessing - StartPostProcessing starts - length of list is " + str(len(pendingusers)))
-        if len(pendingusers) > 0:
-        	randnum = randint(0, len(pendingusers)-1)
-		userloggedin = pendingusers[randnum].get('userloggedin')
-                accesstoken = pendingusers[randnum].get('accesstoken')
-		
-		UserProcessing.objects.filter(userloggedin = userloggedin).delete()
-            	userprocessing = UserProcessing()
-	        userprocessing.userloggedin = UserTomonotomo.objects.get(userid=userloggedin)
-		userprocessing.accesstoken = accesstoken
-		userprocessing.save()
+	def do(self):
+		logger.debug("social_auth_pipeline.startPostProcessing - StartPostProcessing starts")
+		pendingusers = UserProcessing.objects.filter(entryaddtime__lte=datetime.now(pytz.timezone('America/Chicago'))+timedelta(minutes=-5)).values('userloggedin','accesstoken')
+		logger.debug("social_auth_pipeline.startPostProcessing - StartPostProcessing starts - length of list is " + str(len(pendingusers)))
+		if len(pendingusers) > 0:
+			randnum = randint(0, len(pendingusers)-1)
+			userloggedin = pendingusers[randnum].get('userloggedin')
+			accesstoken = pendingusers[randnum].get('accesstoken')
+			
+			UserProcessing.objects.filter(userloggedin = userloggedin).delete()
+			userprocessing = UserProcessing()
+			userprocessing.userloggedin = UserTomonotomo.objects.get(userid=userloggedin)
+			userprocessing.accesstoken = accesstoken
+			userprocessing.save()
 
-                try:
-			logger.debug("social_auth_pipeline.startPostProcessing - Starting Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
-                	postProcessing(userloggedin, accesstoken)
-			logger.debug("social_auth_pipeline.startPostProcessing - Almost Completed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
-                	UserProcessing.objects.filter(userloggedin = userloggedin).delete()
-                	logger.debug("social_auth_pipeline.startPostProcessing - Completed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
-        	except:
-            		logger.exception("social_auth_pipeline.startPostProcessing - Failed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
+			try:
+				logger.debug("social_auth_pipeline.startPostProcessing - Starting Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
+				postProcessing(userloggedin, accesstoken)
+				logger.debug("social_auth_pipeline.startPostProcessing - Almost Completed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
+				UserProcessing.objects.filter(userloggedin = userloggedin).delete()
+				logger.debug("social_auth_pipeline.startPostProcessing - Completed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
+			except:
+				logger.exception("social_auth_pipeline.startPostProcessing - Failed Post Processing for " + str(userloggedin) + " with accesstoken " + accesstoken)
 
-		logger.debug("social_auth_pipeline.startPostProcessing - Before this operation - length of list was " + str(len(pendingusers)))
+			logger.debug("social_auth_pipeline.startPostProcessing - Before this operation - length of list was " + str(len(pendingusers)))
 
-    	return
+		return
 
 class updateQuota(CronJobBase):
 	RUN_AT_TIMES = ['00:00']
@@ -396,12 +396,13 @@ class updateQuota(CronJobBase):
 		users = UserTomonotomo.objects.all().exclude(email=None).values('userid')
 		for user in users:
 			try:
-                		userquota = UserQuota.objects.get(userid=user['userid'])
+				userquota = UserQuota.objects.get(userid=user['userid'])
 				userquota.quota=20
-        		except UserQuota.DoesNotExist:
-                		userquota = UserQuota(userid=user['userid'], quota=30)
-                	userquota.save()
+			except UserQuota.DoesNotExist:
+				userquota = UserQuota(userid=user['userid'], quota=30)
+			userquota.save()
 
 		logger.debug("social_auth_pipeline.updateQuota - Done UpdateQuota")
 		return
+
 
